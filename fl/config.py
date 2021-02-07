b2731f08d6cc94bbcb1ca32cdadbf371c22578e4
sysu101 <suqd@mail2.sysu.edu.cn>
2021-02-07 22:40:37 +0800

    feat: 在数据划分函数中添加采样大小选项

--- fl/config.py
+++ fl/config.py
@@ -1,3 +1,4 @@
 client_num = 4
 data_distribution = [0.2, 0.25, 0.25, 0.3]
 rand_seed = 2021
+sample_num = 1000
--- fl/data.py
+++ fl/data.py
@@ -25,17 +25,17 @@ def gen_partitions(data, indexes, sizes):
     dataset.append(partition)
   return dataset
 
-def gen_iid_dataset(data, seed = config.rand_seed, sizes = config.data_distribution):
+def gen_iid_dataset(data, seed = config.rand_seed, sample_num = config.sample_num, sizes = config.data_distribution):
   engine = Random()
   engine.seed(seed)
-  idx = engine.sample(range(len(data)), 1000)
+  idx = engine.sample(range(len(data)), sample_num)
   engine.shuffle(idx)
   return gen_partitions(data, idx, sizes)
 
-def gen_non_iid_dataset(data, seed = config.rand_seed, sizes = config.data_distribution):
+def gen_non_iid_dataset(data, seed = config.rand_seed, sample_num = config.sample_num, sizes = config.data_distribution):
   engine = Random()
   engine.seed(seed)
-  idx = engine.sample(range(len(data)), 1000)
+  idx = engine.sample(range(len(data)), sample_num)
   idx = [(i, data[i][1]) for i in idx]
   idx.sort(key=lambda ele: ele[1])
   idx = [ele[0] for ele in idx]
